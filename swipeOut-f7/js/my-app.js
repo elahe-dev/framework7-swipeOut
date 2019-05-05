eca90dc4afbaecef4c5c8fa609ef52b1f2d99e5a
Elahe <48132702+elahe-dev@users.noreply.github.com>
2019-05-05 22:48:59 +0430

    Add files via upload

---
+++ swipeOut-f7/js/my-app.js
@@ -0,0 +1,44 @@
+﻿var app = new Framework7({
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'My App',
+    // App id
+    id: 'com.myapp.test',
+    // Enable swipe panel
+    panel: {
+        swipe: 'left',
+    },
+    // Add default routes
+    routes: [
+        {
+            path: '/p4/',
+            pageName: 'page4',
+            on: { pageAfterIn: function (e, page) { } }
+        },
+    ],
+    // ... other parameters
+});
+
+var mainView = app.views.create('.view-main', {
+    stackPages: true,
+    pushState: true,
+});
+//==================================================
+$(document).on('page:beforein', function (e) {
+    $('.page-content').scrollTop(0);
+});
+$('.page-content').on('scroll', function () {
+    //alert(0);
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop > 100) {
+        $(".scroll-top").css('display', 'block');
+    }
+    else { $(".scroll-top").css('display', 'none'); }
+});
+
+$(".scroll-top").click(function () {
+    $('.page-content').scrollTop(0, 600);
+    $(this).css('display', 'none');
+});
+//==================================================b 
